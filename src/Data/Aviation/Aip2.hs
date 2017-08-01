1b7fe6dac24b27070eaa84fd7db60716d3765034
Tony Morris <tmorris@tmorris.net>
2017-08-01 13:43:35 +1000

    aip

--- src/Data/Aviation/Aip2.hs
+++ src/Data/Aviation/Aip2.hs
@@ -22,7 +22,7 @@ import Control.Monad.Trans.Except
 aipRequest ::
   String
   -> Request String
-aipRequest s = 
+aipRequest s =
   mkRequest GET (URI "http:" (Just (URIAuth "" "www.airservicesaustralia.com" "")) ("/aip/" ++ s) "" "")
 
 aipTree ::
@@ -40,38 +40,66 @@ aipTreePos =
 
 aipTreeTraversal ::
   TagTreePos String
-  -> Aip () () ()
+  -> Aip () () () () () () () ()
 aipTreeTraversal t =
   case t of
-    TagTreePos
-      (TagBranch "a" [("href", href)] [TagLeaf (TagText n)])
-      _
-      [TagLeaf (TagText tx)]
-      _ ->
-        let pdate = do  _ <- space
-                        between (char '(') (char ')') parseAipDate
-        in  case n of
-              "AIP Book" ->
-                let p = do  h <- runParse parseAipHref href
-                            d <- runParse pdate tx
-                            pure (Aip (AipBooks [AipBook n d h ()]) mempty mempty)
-                in  fromMaybe mempty p          
-              "AIP Charts" ->
-                let p = do  h <- runParse parseAipHref href
-                            d <- runParse pdate tx
-                            pure (Aip mempty (AipCharts [AipChart n d h ()]) mempty)
-                in  fromMaybe mempty p
-              "AIP Supplements and AICs" ->
-                let p = do  h <- runParse parseAipPgHref href
-                            pure (Aip mempty mempty (AipSupplementsAICs [AipSupplementsAIC n h ()]))
-                in  fromMaybe mempty p
-              _ -> 
-                mempty
+    TagTreePos (TagBranch "li" [] [TagBranch "a" [("href", href)] [TagLeaf (TagText n)]]) _ _ _ ->
+      -- AIP Supplements and AICs
+      -- Summary of SUP/AIC Current 17 Aug 17
+      -- Summary of SUP/AIC Current 20 July 17
+      -- Precision Approach Terrain Charts and Type A & Type B Obstacle Charts
+      case n of
+        "AIP Supplements and AICs" ->
+          let p = do  g <- runParse parseAipPgHref href
+                      pure (oneAipSupplementsAIC (AipSupplementsAIC n g ()))
+          in  fromMaybe mempty p
+        'S':'u':'m':'m':'a':'r':'y':' ':'o':'f':' ':'S':'U':'P':'/':'A':'I':'C':' ':'C':'u':'r':'r':'e':'n':'t':' ':r ->
+          oneAipSummarySUP_AIC (AipSummarySUP_AIC n href r ())
+        "Precision Approach Terrain Charts and Type A & Type B Obstacle Charts" ->
+          oneAipPrecisionObstacleChart (AipPrecisionObstacleChart n href ())
+        _ ->
+          mempty
+    TagTreePos (TagBranch "li" [] (TagBranch "a" [("href", href)] [TagLeaf (TagText n)]:TagLeaf (TagText tx):_)) _ _ _ ->
+      -- AIP Book
+      -- AIP Book
+      -- AIP Charts
+      -- Departure and Approach Procedures (DAP)
+      -- Departure and Approach Procedures (DAP)
+      -- Designated Airspace Handbook (DAH)
+      -- En Route Supplement Australia (ERSA)
+      -- En Route Supplement Australia (ERSA)
+      let pdate = do  _ <- space
+                      between (char '(') (char ')') parseAipDate
+      in  case n of
+            "AIP Book" ->
+              let p = do  h <- runParse parseAipHref href
+                          d <- runParse pdate tx
+                          pure (oneAipBook (AipBook n d h ()))
+              in  fromMaybe mempty p   
+            "AIP Charts" ->
+              let p = do  h <- runParse parseAipHref href
+                          d <- runParse pdate tx
+                          pure (oneAipChart (AipChart n d h ()))
+              in  fromMaybe mempty p
+            "Departure and Approach Procedures (DAP)" ->
+              let p = do  h <- runParse parseAipHref href
+                          d <- runParse pdate tx
+                          pure (oneAipDAP (AipDAP n d h ()))
+              in  fromMaybe mempty p
+            "Designated Airspace Handbook (DAH)" ->
+              oneAipDAH (AipDAH n href ())
+            "En Route Supplement Australia (ERSA)" ->
+              let p = do  h <- runParse parseAipHref href
+                          d <- runParse pdate tx
+                          pure (oneAipERSA (AipERSA n d h ()))
+              in  fromMaybe mempty p   
+            _ ->
+              mempty
     _ ->
       mempty
 
 getAipTree ::
-  ExceptT ConnError IO (Aip () () ())
+  ExceptT ConnError IO (Aip () () () () () () () ())
 getAipTree =
   traverseTree aipTreeTraversal <$> aipTreePos
 
@@ -82,37 +110,87 @@ data Link =
     String
   deriving (Eq, Ord, Show)
 
-data Aip books charts supplementsaics =
+data Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts =
   Aip
     (AipBooks books)
     (AipCharts charts)
     (AipSupplementsAICs supplementsaics)
-    -- etc
+    (AipSummarySUP_AICs summarysupaics)
+    (AipDAPs daps)
+    (AipDAHs dahs)
+    (AipERSAs ersas)
+    (AipPrecisionObstacleCharts precisionobstaclecharts)
   deriving (Eq, Ord, Show)
 
-instance Monoid (Aip books charts supplementsaics) where
-  Aip a1 a2 a3 `mappend` Aip b1 b2 b3 =
-    Aip (a1 `mappend` b1) (a2 `mappend` b2) (a3 `mappend` b3)
+instance Monoid (Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts) where
+  Aip a1 a2 a3 a4 a5 a6 a7 a8 `mappend` Aip b1 b2 b3 b4 b5 b6 b7 b8 =
+    Aip
+      (a1 `mappend` b1)
+      (a2 `mappend` b2)
+      (a3 `mappend` b3)
+      (a4 `mappend` b4)
+      (a5 `mappend` b5)
+      (a6 `mappend` b6)
+      (a7 `mappend` b7)
+      (a8 `mappend` b8)
   mempty =
-    Aip mempty mempty mempty
-    
-data AipSupplementsAICs a =
-  AipSupplementsAICs
-    [AipSupplementsAIC a]
-  deriving (Eq, Ord, Show)
+    Aip
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
 
-data AipSupplementsAIC a =
-  AipSupplementsAIC
-    String
-    AipPg
-    a
-  deriving (Eq, Ord, Show)
+oneAipBook ::
+  AipBook books
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipBook book =
+  Aip (AipBooks [book]) mempty mempty mempty mempty mempty mempty mempty
 
-instance Monoid (AipSupplementsAICs a) where
-  AipSupplementsAICs x `mappend` AipSupplementsAICs y =
-    AipSupplementsAICs (x `mappend` y)
-  mempty =
-    AipSupplementsAICs mempty
+oneAipChart ::
+  AipChart charts
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipChart chart =
+  Aip mempty (AipCharts [chart]) mempty mempty mempty mempty mempty mempty
+
+oneAipSupplementsAIC ::
+  AipSupplementsAIC supplementsaics
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipSupplementsAIC supplementsaic =
+  Aip mempty mempty (AipSupplementsAICs [supplementsaic]) mempty mempty mempty mempty mempty
+
+oneAipSummarySUP_AIC ::
+  AipSummarySUP_AIC summarysupaics
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipSummarySUP_AIC summarysupaic =
+  Aip mempty mempty mempty (AipSummarySUP_AICs [summarysupaic]) mempty mempty mempty mempty
+
+oneAipDAP ::
+  AipDAP daps
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipDAP dap =
+  Aip mempty mempty mempty mempty (AipDAPs [dap]) mempty mempty mempty
+
+oneAipDAH ::
+  AipDAH dahs
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipDAH dah =
+  Aip mempty mempty mempty mempty mempty (AipDAHs [dah]) mempty mempty
+
+oneAipERSA ::
+  AipERSA ersas
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipERSA ersa =
+  Aip mempty mempty mempty mempty mempty mempty (AipERSAs [ersa]) mempty
+
+oneAipPrecisionObstacleChart ::
+  AipPrecisionObstacleChart precisionobstaclecharts
+  -> Aip books charts supplementsaics summarysupaics daps dahs ersas precisionobstaclecharts
+oneAipPrecisionObstacleChart precisionobstaclechart =
+  Aip mempty mempty mempty mempty mempty mempty mempty (AipPrecisionObstacleCharts [precisionobstaclechart])
 
 data AipBooks a =
   AipBooks
@@ -151,6 +229,117 @@ data AipChart a =
     AipHref
     a
   deriving (Eq, Ord, Show)
+
+data AipSupplementsAICs a =
+  AipSupplementsAICs
+    [AipSupplementsAIC a]
+  deriving (Eq, Ord, Show)
+
+data AipSupplementsAIC a =
+  AipSupplementsAIC
+    String
+    AipPg
+    a
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipSupplementsAICs a) where
+  AipSupplementsAICs x `mappend` AipSupplementsAICs y =
+    AipSupplementsAICs (x `mappend` y)
+  mempty =
+    AipSupplementsAICs mempty
+
+data AipSummarySUP_AICs a =
+  AipSummarySUP_AICs
+    [AipSummarySUP_AIC a]
+  deriving (Eq, Ord, Show)
+
+data AipSummarySUP_AIC a =
+  AipSummarySUP_AIC
+    String
+    String
+    String
+    a
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipSummarySUP_AICs a) where
+  AipSummarySUP_AICs x `mappend` AipSummarySUP_AICs y =
+    AipSummarySUP_AICs (x `mappend` y)
+  mempty =
+    AipSummarySUP_AICs mempty
+
+data AipDAPs a =
+  AipDAPs
+    [AipDAP a]
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipDAPs a) where
+  AipDAPs x `mappend` AipDAPs y =
+    AipDAPs (x `mappend` y)
+  mempty =
+    AipDAPs mempty
+
+data AipDAP a =
+  AipDAP
+    String
+    AipDate
+    AipHref
+    a
+  deriving (Eq, Ord, Show)
+
+data AipDAHs a =
+  AipDAHs
+    [AipDAH a]
+  deriving (Eq, Ord, Show)
+
+data AipDAH a =
+  AipDAH
+    String
+    String
+    a
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipDAHs a) where
+  AipDAHs x `mappend` AipDAHs y =
+    AipDAHs (x `mappend` y)
+  mempty =
+    AipDAHs mempty
+
+data AipERSAs a =
+  AipERSAs
+    [AipERSA a]
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipERSAs a) where
+  AipERSAs x `mappend` AipERSAs y =
+    AipERSAs (x `mappend` y)
+  mempty =
+    AipERSAs mempty
+
+data AipERSA a =
+  AipERSA
+    String
+    AipDate
+    AipHref
+    a
+  deriving (Eq, Ord, Show)
+
+data AipPrecisionObstacleCharts a =
+  AipPrecisionObstacleCharts
+    [AipPrecisionObstacleChart a]
+  deriving (Eq, Ord, Show)
+
+data AipPrecisionObstacleChart a =
+  AipPrecisionObstacleChart
+    String
+    String
+    a
+  deriving (Eq, Ord, Show)
+
+instance Monoid (AipPrecisionObstacleCharts a) where
+  AipPrecisionObstacleCharts x `mappend` AipPrecisionObstacleCharts y =
+    AipPrecisionObstacleCharts (x `mappend` y)
+  mempty =
+    AipPrecisionObstacleCharts mempty
 
 ----
 
